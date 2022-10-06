6e550c965aac6d00799024ffb5a61591d5e7af8e
Christian Nally <sticksallison@gmail.com>
2022-10-06 11:45:13 -0700

    in-class edits

---
+++ hash.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+
+const plaintextPassword = 'abcd';
+
+// bcrypt.genSalt(10, function(err, salt) {
+// console.log('salt: ',salt);
+//   bcrypt.hash(plaintextPassword, salt, function(err, hash) {
+//     console.log('hash:',hash);
+//   });
+// });
+
+bcrypt.genSalt(10)
+  .then((salt) => {
+    console.log('salt', salt);
+    return bcrypt.hash(plaintextPassword, salt);
+  })
+  .then((hash) => {
+    console.log('hash', hash);
+  });
+
+const hash = '$2b$10$raF43.U/imefjqaj.ShhMeSij6KUpuwI9wLN47lZW8a22FlOQbdle';
+
+bcrypt.compare('abcd', hash)
+  .then((result) => {
+    console.log('do the passwords match?', result);
+  });
